0fb373d744315c8e9ea37c1984d87f6268a707c3
devDounetsTuan <45481311+devDounetsTuan@users.noreply.github.com>
2019-01-18 16:14:55 +0700

    app simple chat

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+const server = app.listen(5000, () => {
+    console.log(`App running on port 5000`);
+})
+const io = require('socket.io').listen(server);
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('chatter', (message) => {
+      console.log('chatter : ', message)
+      io.emit('chatter', message)
+    })
+  })
