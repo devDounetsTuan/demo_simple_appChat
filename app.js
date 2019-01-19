5ed2b47cf6b48a97b48caeeaa6a352325bb2bfa6
Cracker <tuanbsonnet@gmail.com>
2019-01-19 18:29:02 +0700

    Add cool face API

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const APP_PORT = 5000 || process.env.PORT;
+const APP_PORT = process.env.PORT || 5000;
 
 const server = app.listen(APP_PORT, () => {
     console.log(`App running on port 5000`);
