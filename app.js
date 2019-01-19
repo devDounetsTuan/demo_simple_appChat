c7f3a58884ffd2c60a49b499868c87aba4efa6f6
Cracker <tuanbsonnet@gmail.com>
2019-01-19 16:45:01 +0700

    Added a Procfile.

--- app.js
+++ app.js
@@ -25,10 +25,3 @@ io.on('connection', (socket) => {
     })
   })
 
-  express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
